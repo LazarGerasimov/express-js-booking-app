2b0f8e54c3e2581d18f9d12dc3f87c0bdc429e87
Laz <lazgerasimov@gmail.com>
2022-12-20 18:22:59 +0000

    add create hotel functionality

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -1,3 +1,6 @@
+const { create } = require('../services/hotelService');
+const { parseError } = require('../util/parser');
+
 const hotelController = require('express').Router();
 
 hotelController.get('/:id/details', (req, res) => {
@@ -10,6 +13,26 @@ hotelController.get('/create', (req, res) => {
     res.render('create', {
         title: 'Create Hotel'
     });
+});
+
+hotelController.post('/create', async (req, res) => {
+    const hotel = {
+        name: req.body.name,
+        city: req.body.city,
+        imageUrl: req.body.imageUrl,
+        rooms: Number(req.body.rooms),
+        owner: req.user._id
+    };
+    try {
+        await create(hotel);
+        res.redirect('/');
+    } catch (err) {
+        res.render('create', {
+            title: 'Create Hotel',
+            body: hotel,
+            errors: parseError(err)
+        })
+    }
 });
 
 hotelController.get('/:id/edit', (req, res) => {
